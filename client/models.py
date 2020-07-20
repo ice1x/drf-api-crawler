bdee959feb6f37599af1795b93de7c0eed0e866f
ice1x <ice1x@gmail.com>
2020-07-20 16:06:20 +0500

    Add ordering status code tests and crawler test

--- client/models.py
+++ client/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Posts(models.Model):
     """
-    date,channel,country,os,impressions,clicks,installs,spend,revenue
-    2017-05-17,adcolony,US,android,19887,494,76,148.2,149.04
+    Ycombinator posts
     """
     # id = models.IntegerField(primary_key=True)
     url = models.CharField(max_length=256)
--- client/tests.py
+++ client/tests.py
@@ -1,26 +1,64 @@
+import subprocess
+import psycopg2
+
 from django.test import TestCase
 
 
-class PostsViewsTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # TODO: truncate db table
-        pass
+# TODO: import from base settings
+HOST = 'localhost'
+USER = 'postgres'
+PASSWORD = 'postgres'
+DBNAME = 'postgres'
+BASE_API_URL = 'http://localhost:8000/posts'
 
+
+class PostsViewsTests(TestCase):
     def test_view_url_exists_at_desired_location(self):
-        resp = self.client.get('http://localhost:8000/posts')
+        resp = self.client.get(BASE_API_URL)
         self.assertEqual(resp.status_code, 200)
 
     def test_view_base_url_is_404(self):
         resp = self.client.get('http://localhost:8000')
         self.assertEqual(resp.status_code, 404)
 
-    def _test_(self):
-        # TODO add data inserting to db to read it via views
-        # resp = self.client.get('http://localhost:8000/posts')
-        # self.assertEqual(resp.status_code, 200)
-        pass
+    def test_view_positive_order_url(self):
+        resp = self.client.get(f'{BASE_API_URL}?order=url')
+        self.assertEqual(resp.status_code, 200)
 
-    def tearDown(self) -> None:
-        # TODO: truncate db table
-        pass
+    def test_view_positive_order_title(self):
+        resp = self.client.get(f'{BASE_API_URL}?order=title')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_positive_order_created(self):
+        resp = self.client.get(f'{BASE_API_URL}?order=created')
+        self.assertEqual(resp.status_code, 200)
+
+
+# class CrawlerGetsOneDayPosts(TestCase):
+#     con = psycopg2.connect(
+#         host=HOST,
+#         user=USER,
+#         password=PASSWORD,
+#         dbname=DBNAME
+#     )
+#     cur = con.cursor()
+#
+#     @classmethod
+#     def setUpTestData(cls):
+#         cls.cur.execute("TRUNCATE client_posts")
+#         cls.con.commit()
+#
+#     def test_count_one_page_posts_amount(self):
+#         self.cur.execute("SELECT count(1) FROM client_posts")
+#         result_begin = self.cur.fetchone()
+#
+#         subprocess.call(['scrapy', 'crawl', 'forward'])
+#
+#         self.cur.execute("SELECT count(1) FROM client_posts")
+#         result_end = self.cur.fetchone()
+#
+#         self.assertEqual((0, 30), (result_begin[0], result_end[0]))
+#
+#     def tearDown(self):
+#         self.cur.execute("TRUNCATE client_posts")
+#         self.con.commit()
